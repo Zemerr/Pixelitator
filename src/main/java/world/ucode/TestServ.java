c368607ce3fb618666fa9f9702a49d178da7cb21
Oleksandr Stanislavskyi <ostanislav@e1r8p3.unit.ua>
2020-10-17 21:22:07 +0300

    first

---
+++ src/main/java/world/ucode/TestServ.java
@@ -0,0 +1,15 @@
+package world.ucode;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TestServ extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        resp.getWriter().write("mazafaka");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ostanislav
+  Date: 10/17/20
+  Time: 3:09 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>My first app</title>
+  </head>
+  <body>
+  $END lol$
+  </body>
+</html>
